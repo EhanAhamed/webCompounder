9b8e7eeeba8919d2a0be79d785e52ab95b8a36d6
Ehan <ehanahamed@ehan.dev>
2022-07-11 23:47:26 -0400

    🚧 Parse Config File From CLI Argument

--- bin/index.js
+++ bin/index.js
@@ -6,6 +6,8 @@ const fs = require("fs");
 const usage = "\n" + "Usage: webCompounder ./pathTo/configFile.json";
 const options = yargs.usage(usage).help(true).argv;
 
-const configFile = JSON.parse(fs.readFileSync(yargs.argv._[0], { encoding: "utf-8" }));
+const configJson = JSON.parse(
+  fs.readFileSync(yargs.argv._[0], { encoding: "utf-8" })
+);
 
 console.log(configFile.name)
